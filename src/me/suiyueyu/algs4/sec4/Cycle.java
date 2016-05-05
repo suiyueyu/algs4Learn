0f6bc1c418c387a615b57d078ee5901b82c14c87
suiyueyu <suiyueyu@gmail.com>
2016-05-05 22:03:11 +0800

    20160505 图论2

---
+++ src/me/suiyueyu/algs4/sec4/Cycle.java
@@ -0,0 +1,33 @@
+package me.suiyueyu.algs4.sec4;
+
+/**
+ * Created by yzcc on 2016/5/4.
+ */
+public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle;
+
+    public Cycle(Graph_model G) {
+        marked = new boolean[G.V()];
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) {
+                dfs(G, s, s);
+            }
+        }
+    }
+
+    private void dfs(Graph_model G, int v, int u) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w, v);
+            } else if (w != u) {
+                hasCycle = true;
+            }
+        }
+    }
+
+    public boolean hasCycle() {
+        return hasCycle;
+    }
+}
